720eb527da7b7425850f7ebad8bb09685df4a897
vovapasko <vovapasko1699@gmail.com>
2025-06-19 13:37:51 +0200

    feat(frontend): provided dummy chat window

--- vibe-coding-messenger-frontend/src/App.js
+++ vibe-coding-messenger-frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function WriteMessage() {
+    function handleSubmit(e) {
+        // Prevent the browser from reloading the page
+        e.preventDefault();
 
-export default App;
+        // Read the form data
+        const form = e.target;
+        const formData = new FormData(form);
+
+
+        // Or you can work with it as a plain object:
+        const formJson = Object.fromEntries(formData.entries());
+        // Save the name to sessionStorage
+        if (formJson.chatterName && formJson.chatterName !== "Anonymous") {
+            sessionStorage.setItem("chatterName", formJson.chatterName);
+        }
+        console.log(formJson);
+    }
+
+    function getValueFromSession() {
+        // checks if there is a participant name in the session storage,
+        // if there is, return the participant name
+        // if there isn't, return an "Anonymous"
+        if (!sessionStorage.getItem("chatterName")) {
+            return "Anonymous";
+        }
+        return sessionStorage.getItem("chatterName");
+    }
+
+    return (
+        <form method="post" onSubmit={handleSubmit}>
+            <label>
+                Name: <input name="chatterName" defaultValue={getValueFromSession()}/>
+            </label>
+            <label>
+                Write message >
+                <textarea
+                    name="messageContent"
+                    rows={4}
+                    cols={40}
+                />
+            </label>
+            <hr/>
+            <button type="submit">Send</button>
+        </form>
+    );
+}
