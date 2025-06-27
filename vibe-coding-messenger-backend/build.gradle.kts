f84d1823481bfe82f5650f945b67139136736a05
Vladimir Pasko <vladimir.pasko@beyonnex.io>
2025-06-27 17:44:08 +0200

    feat(backend): provided basic message echoing

--- vibe-coding-messenger-backend/build.gradle.kts
+++ vibe-coding-messenger-backend/build.gradle.kts
@@ -20,7 +20,6 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-serialization-jackson")
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-websockets")
@@ -28,6 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
